74f7b158b33b0840b97574a51f9e49d5ea87613e
Tushar Saxena <stushar345@gmail.com>
2021-03-03 15:10:31 +0530

    Add files via upload

--- two.cpp
+++ two.cpp
@@ -1,27 +1,66 @@
-Node* abc(int in[],int pre[],int is,int ie,int &preIndex)
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node 
 {
-    if(is>ie)
+	int data; 
+	Node* left;
+	Node* right;
+};
+
+Node* GetNewNode(int data) 
+{
+	Node* newNode = new Node();
+	newNode->data = data;
+	newNode->left = newNode->right = NULL;
+	return newNode;
+}
+
+Node* Insert(Node* root,int data) 
+{
+	if(root == NULL)                                      // empty tree
+    {                                           
+		root = GetNewNode(data);
+	}
+	
+	else if(data <= root->data)                         // if data to be inserted start lesser, insert inorder left subtree. 
+    {
+		root->left = Insert(root->left,data);
+	}
+	
+	else                                                // else, insert inorder right subtree. 
+    {
+		root->right = Insert(root->right,data);
+	}
+	return root;
+}
+
+Node* inorder_and_preorder(int inorder[],int preorder[],int start,int end,int &preorder_index)
+{
+    if(start>end)
     {
         return NULL;
     }
-    Node *root=new Node(pre[preIndex++]);
+    Node* root = GetNewNode(preorder[preorder_index++]);
     int index;
-    for(int i=is;i<=ie;i++)
+    for(int i=start;i<=end;i++)
     {
-        if(in[i]==root->data)
+        if(inorder[i]==root->data)
         {
          index=i;
         break;
         }
     }
-    root->left=abc(in,pre,is,index-1,preIndex);
-    root->right=abc(in,pre,index+1,ie,preIndex);
+    root->left=inorder_and_preorder(inorder,preorder,start,index-1,preorder_index);
+    root->right=inorder_and_preorder(inorder,preorder,index+1,end,preorder_index);
 return root;
 }
-Node* buildTree(int in[],int pre[], int n)
+
+Node* buildTree(int inorder[],int preorder[], int n)
 {
-    int preIndex=0;
-    int is=0;int ie=n-1;
-Node *root1=abc(in,pre,is,ie,preIndex);
-return root1;
+    int preorder_index=0;
+    int start=0;
+	int end=n-1;
+	Node *root1=inorder_and_preorder(inorder,preorder,start,end,preorder_index);
+	return root1;
 }
